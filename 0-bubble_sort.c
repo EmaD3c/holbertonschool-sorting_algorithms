41ad10615c2609cebae50c142c3ef91ee2e1af76
Ema Decot <emadecot03@gmail.com>
2024-08-07 17:42:29 +0200

    add a cindition if the array is already sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,37 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * bubble_sort - Trie un tableau d'entiers en ordre croissant
- * en utilisant le tri à bulles
+ * print_array - Imprime un tableau d'entiers
+ * @array: Le tableau à imprimer
+ * @size: Le nombre d'éléments dans le tableau
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * bubble_sort - Trie un tableau d'entiers
+ * en ordre croissant en utilisant le tri à bulles
  * @array: Le tableau à trier
  * @size: La taille du tableau
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	int temp;
+	int swapped;
 
-	/* Vérifie si le tableau est NULL ou contient moins de 2 éléments */
 	if (array == NULL || size < 2)
 		return;
 
 	/* Boucle à travers chaque élément du tableau */
 	for (i = 0; i < size - 1; i++)
 	{
-		/* Compare les éléments adjacents et les échange si nécessaire */
+		swapped = 0;
+
+		/* Effectue la boucle interne pour comparer les éléments adjacents */
 		for (j = 0; j < size - i - 1; j++)
 		{
 			/* Si l'élément actuel est plus grand que le suivant, échangez-les */
 			if (array[j] > array[j + 1])
 			{
-				tmp = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				array[j + 1] = temp;
+				swapped = 1;
 
 				/* Imprime le tableau après chaque échange */
 				print_array(array, size);
 			}
 		}
+
+		/* Si aucun élément n'a été échangé alors le tableau est trié */
+		if (!swapped)
+			break;
 	}
 }
