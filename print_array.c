e084338250fe65cec42498569f058510e261b942
gdbmrm <>
2024-08-09 09:54:44 +0000

    ajout 2-O

--- print_array.c
+++ print_array.c
@@ -9,7 +9,6 @@
  */
 void print_array(const int *array, size_t size)
 {
-<<<<<<< HEAD
   size_t i;
 
   i = 0;
@@ -22,17 +21,3 @@ void print_array(const int *array, size_t size)
   }
   printf("\n");
 }
-=======
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
->>>>>>> Mariama
